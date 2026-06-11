b0aed7c0d202a25ed8f969f3d121181a7fd2fc5c
bhuvanaraj71 <bhuvi02bhuvi@gmail.com>
2026-06-12 01:22:19 +0530

    uploading hooks file

--- tests/Hooks.spec.js
+++ tests/Hooks.spec.js
@@ -2,19 +2,37 @@ import {test, expect} from '@playwright/test'
 
 //adding beforeEach Hook
 let page;
-test.beforeEach(async({browser})=>{
+test.beforeAll(async({browser})=>{
     page = await browser.newPage();
     await page.goto('https://www.demoblaze.com/index.html');
-
-//Login
     await page.locator('#login2').click();
-    await page.locator('#loginusername').fill('bhuvi');
+    await page.locator('#loginusername').fill('123');
     await page.locator('#loginpassword').fill('123');
     await page.getByRole('button',{name:'Log in'}).click();
+})
 
-//click on product and add to cart
-    await page.locator('link', { name: 'Samsung galaxy s6'}).click();
-    await page.getByText('Add to cart').click();
+
+test.afterAll(async()=>{
+    await page.locator('#logout2').click();
     await page.waitForTimeout(3000);
+})
+
+test('Add to Cart', async()=>{
+    await page.locator('//a[contains(text(),"Samsung galaxy s6")]').click();
+
+    page.on('dialog', async dialog =>{
+        expect(dialog.type()).toContain('alert');                    //assertion for dailog type to be alert
+        expect(dialog.message()).toContain('Product added');    //assertion for dailog message to be what's displayed in alert window
+        await dialog.accept();
+    })
+    await page.getByText('Add to cart').click();
 
 })
+
+test('No Of Elements', async()=>{
+    //await page.goto('https://www.demoblaze.com/index.html');
+    const elements = await page.$$('.card-img-top');
+    console.log(elements.length);
+
+})
+
